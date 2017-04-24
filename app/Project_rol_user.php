0d068f6895b5016367dd5b76a5969aa2924e1def
Milka Cabrilla <milka.cabrilla@gmail.com>
2017-04-24 09:46:09 -0500

    Create Project_rol_user.php

---
+++ app/Project_rol_user.php
@@ -0,0 +1,13 @@
+<?php
+namespace App;
+class Project_rol_user
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id_project', 'id_rol', 'id_user',
+    ];
+}
