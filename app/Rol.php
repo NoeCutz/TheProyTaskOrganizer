abba7dcde2ac62b8039330cc634e95ca0b85eabf
Milka Cabrilla <milka.cabrilla@gmail.com>
2017-04-24 09:39:48 -0500

    Create Rol.php

---
+++ app/Rol.php
@@ -0,0 +1,13 @@
+<?php
+namespace App;
+class Rol
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+    ];
+}
