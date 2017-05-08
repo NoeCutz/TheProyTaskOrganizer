2ca6e4f6ca2f5c12cdd66b54a3e0316fe548029f
jerry <jeremiah.seagraves@outlook.com>
2017-05-08 16:27:33 -0500

    Modelos creados

--- app/Review.php
+++ app/Review.php
@@ -1,13 +1,21 @@
 <?php
 namespace App;
-class Task
+use Illuminate\Database\Eloquent\Model;
+
+class Review extends Model
 {
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
-    protected $fillable = [
-        'title', 'description',
-    ];
+    protected $fillable = ['title', 'description'];
+
+    public function task(){
+        return $this->belongsTo(Task::class);
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Task.php
+++ app/Task.php
@@ -1,16 +1,16 @@
 <?php
 namespace App;
 use App\Project;
-class Task
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
 {
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
-    protected $fillable = [
-        'id_project', 'id_user', 'id_rol', 'name', 'description', 'state',
-    ];
+    protected $fillable = ['id_project', 'id_user', 'id_rol', 'name', 'description', 'state'];
 
     public function project(){
       return $this->belongsTo(Project::class);
@@ -18,5 +18,9 @@ class Task
 
     public function user(){
       return $this->belongsTo(Project::class);
+    }
+
+    public function role(){
+        return $this->hasOne(Role::class);
     }
 }
