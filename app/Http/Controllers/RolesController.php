9c0de90c43e9c6b91de25f53ccc9d50d6026cd06
NoeCutz <noecutz03@gmail.com>
2017-05-22 12:00:35 -0500

    Creado el controlador de roles

---
+++ app/Http/Controllers/RolesController.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Response;
+use App\Project;
+use App\Role;
+use App\Http\Requests\StoreRoleProjectPost;
+class RolesController extends Controller
+{
+    public function index(Project $project){
+    return Response::json($project->load('roles'));
+    }
+    
+    public function storeReview(Project $project,StoreRoleProjectPost $request)
+    {
+       $attributes = $request->input('name');
+        $role= Role::create($attributes);
+        $project_id = $project-> id;
+        $role ->project() -> associate($project_id);
+        $role -> save();
+       return Response::json($role->load('roles'));
+    }
+    
+}
